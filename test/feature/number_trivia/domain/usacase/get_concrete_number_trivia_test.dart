d55579d6dd2c7109c168d8dbb2bf8d224acad2e7
Armağan Gök <1armagangok@gmail.com>
2022-10-31 20:57:07 +0300

    structure implemented

---
+++ test/feature/number_trivia/domain/usacase/get_concrete_number_trivia_test.dart
@@ -0,0 +1,37 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tdd_clean_architecture/feature/number_trivia/domain/entities/number_trivia.dart';
+import 'package:tdd_clean_architecture/feature/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:tdd_clean_architecture/feature/number_trivia/domain/usecase/get_concrete_number_trivia.dart';
+
+class MockNumberTriviaRepository extends Mock implements NumberTriviaRepository {}
+
+void main() {
+  late GetConcreteNumberTrivia usecase;
+  late MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  const tNumber = 1;
+  const tNumberTrivia = NumberTrivia(number: 1, text: 'test');
+
+  test(
+    'should get trivia for the number from the repository',
+    () async {
+      when(mockNumberTriviaRepository.getConcreteNumberTrivia(1))
+          .thenAnswer((_) async => const Right(tNumberTrivia));
+      // The "act" phase of the test. Call the not-yet-existent method.
+      final result = await usecase.call(number: tNumber);
+      // UseCase should simply return whatever was returned from the Repository
+      expect(result, const Right(tNumberTrivia));
+      // Verify that the method has been called on the Repository
+      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      // Only the above method should be called and nothing more.
+      verifyNoMoreInteractions(mockNumberTriviaRepository);
+    },
+  );
+}
