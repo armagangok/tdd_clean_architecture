f99aa212099d849aded90a35d72ad4f2e9aff325
Armağan Gök <1armagangok@gmail.com>
2022-10-31 21:58:22 +0300

    domain test implemented

--- lib/feature/number_trivia/domain/usecase/get_concrete_number_trivia.dart
+++ lib/feature/number_trivia/domain/usecase/get_concrete_number_trivia.dart
@@ -1,15 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
 import '../../../../core/error/failures.dart';
+import '../../../../core/usecase/usecase.dart';
 import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, int> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
-
-  Future<Either<Failure, NumberTrivia>> call({required int number}) async {
-    return await repository.getConcreteNumberTrivia(number);
+  
+  @override
+  Future<Either<Failure, NumberTrivia>> call(int params) async{
+    return await repository.getConcreteNumberTrivia(params);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+  const Params({required this.number});
+
+  @override
+  List<Object?> get props => [number];
 }
--- test/feature/number_trivia/domain/usacase/get_random_number_triva_test.dart
+++ test/feature/number_trivia/domain/usacase/get_random_number_triva_test.dart
@@ -3,30 +3,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:tdd_clean_architecture/feature/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tdd_clean_architecture/feature/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:tdd_clean_architecture/feature/number_trivia/domain/usecase/get_concrete_number_trivia.dart';
+import 'package:tdd_clean_architecture/feature/number_trivia/domain/usecase/get_random_number_triva.dart';
 
-class MockNumberTriviaRepository extends Mock
-    implements NumberTriviaRepository {}
+class MockNumberTriviaRepository extends Mock implements NumberTriviaRepository {}
 
 void main() {
-  late GetConcreteNumberTrivia usecase;
+  late GetRandomNumberTrivia usecase;
   late MockNumberTriviaRepository mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+    usecase = GetRandomNumberTrivia(repository: mockNumberTriviaRepository);
   });
 
   const tNumber = 1;
   const tNumberTrivia = NumberTrivia(number: 1, text: 'test');
 
   test(
-    'should get trivia for the number from the repository',
+    'should get trivia from the repository',
     () async {
-      when(mockNumberTriviaRepository.getConcreteNumberTrivia(1))
+      when(mockNumberTriviaRepository.getRandomNumberTrivia())
           .thenAnswer((_) async => const Right(tNumberTrivia));
       // The "act" phase of the test. Call the not-yet-existent method.
-      final result = await usecase.call(tNumber);
+      final result = await usecase.call(NoParams());
       // UseCase should simply return whatever was returned from the Repository
       expect(result, const Right(tNumberTrivia));
       // Verify that the method has been called on the Repository
